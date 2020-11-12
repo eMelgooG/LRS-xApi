b0e8f203bf14c5b9f0702b61400d3d29a79d2a7b
Emanuel Adrian Lucaci <manu_bc2004@hotmail.com>
2020-11-12 07:19:49 +0200

    Create [RequiredVersionHeader] attribute filter

--- xApi/Controllers/AboutController.cs
+++ xApi/Controllers/AboutController.cs
@@ -5,16 +5,19 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using xApi.Data;
+using xApi.Filters;
 
 namespace xApi.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
+    [RequiredVersionHeader]
     [Route("xapi/about")]
     public class AboutController : ApiController
     {
         [HttpGet]   
         public IHttpActionResult GetAbout()
         {
+   
             var about = About.Instance;
             return Ok(about);
         }
---
+++ xApi/Filters/RequiredVersionHeaderAttribute.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http.Results;
+using xApi.Data.Helpers;
+
+namespace xApi.Filters
+{
+    public class RequiredVersionHeaderAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext context)
+        {
+            var supported = ApiVersion.GetSupported();
+            try
+            {
+                if (!context.Request.Headers.Contains(ApiHeaders.XExperienceApiVersion))
+                {
+                    throw new Exception($"Missing '{ApiHeaders.XExperienceApiVersion}' header.");
+                }
+
+                IEnumerable<string> headerValues = context.Request.Headers.GetValues(ApiHeaders.XExperienceApiVersion);
+                string requestVersion = headerValues.FirstOrDefault();
+                if (string.IsNullOrEmpty(requestVersion))
+                {
+                    throw new Exception($"'{ApiHeaders.XExperienceApiVersion}' header or it's null or empty.");
+                }
+
+                try
+                {
+                    ApiVersion version = requestVersion;
+                }
+                catch (Exception)
+                {
+                    throw new Exception($"'{ApiHeaders.XExperienceApiVersion}' header is '{requestVersion}' which is not supported.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var response = context.Request.CreateResponse(System.Net.HttpStatusCode.BadRequest, ex.Message + " Supported Versions are: " + string.Join(", ", supported.Select(x => x.Key)));
+                context.Response = response;
+                context.Response.Headers.Add(ApiHeaders.XExperienceApiVersion, ApiVersion.GetLatest().ToString());
+   
+            }
+        }
+    }
+    }
+
