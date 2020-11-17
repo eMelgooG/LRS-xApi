9e57376b97be3682d1b6b1069a1a79e7dd450d82
Emanuel Adrian Lucaci <manu_bc2004@hotmail.com>
2020-11-17 11:03:00 +0200

    Create RawBody Attribute
    Created RawBody attribute to be able to receive raw bytes data and to bypass the Content-Type 415 error for the ActivityProfile Put and Post method

---
+++ xApi/Filters/RawBody/RawBodyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace xApi.Filters.RawBody
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public sealed class RawBodyAttribute : ParameterBindingAttribute
+    {
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentException("Invalid parameter");
+
+            return new RawBodyParameterBinding(parameter);
+        }
+    }
+}
---
+++ xApi/Filters/RawBody/RawBodyParameterBinding.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+
+namespace xApi.Filters.RawBody
+{
+    /// <summary>
+    /// Reads the Request body into a string/byte[] and
+    /// assigns it to the parameter bound.
+    /// 
+    /// Should only be used with a single parameter on
+    /// a Web API method using the [NakedBody] attribute
+    /// </summary>
+    public class RawBodyParameterBinding : HttpParameterBinding
+    {
+        public RawBodyParameterBinding(HttpParameterDescriptor descriptor)
+            : base(descriptor)
+        {
+
+        }
+
+
+        public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider,
+                                                    HttpActionContext actionContext,
+                                                    CancellationToken cancellationToken)
+        {
+            var binding = actionContext
+                .ActionDescriptor
+                .ActionBinding;
+
+            if  (actionContext.Request.Method == HttpMethod.Get)
+                return EmptyTask.Start();
+
+            var type = binding
+                        .ParameterBindings[0]
+                        .Descriptor.ParameterType;
+
+            if (type == typeof(string))
+            {
+                return actionContext.Request.Content
+                        .ReadAsStringAsync()
+                        .ContinueWith((task) =>
+                        {
+                            var stringResult = task.Result;
+                            SetValue(actionContext, stringResult);
+                        });
+            }
+            else if (type == typeof(byte[]))
+            {
+                return actionContext.Request.Content
+                    .ReadAsByteArrayAsync()
+                    .ContinueWith((task) =>
+                    {
+                        byte[] result = task.Result;
+                        SetValue(actionContext, result);
+                    });
+            }
+
+            throw new InvalidOperationException("Only string and byte[] are supported for [NakedBody] parameters");
+        }
+
+        public override bool WillReadBody
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+    }
+}
