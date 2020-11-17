849c60807c983fd689b516f1fec770d1ba243301
Emanuel Adrian Lucaci <manu_bc2004@hotmail.com>
2020-11-17 16:42:25 +0200

    Add ActivityProfileRepository

---
+++ xApi/Repositories/ActivityProfileRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using xApi.Data.Documents;
+
+namespace xApi.Repositories
+{
+    public class ActivityProfileRepository
+    {
+
+        public ActivityProfileRepository()
+        {
+      
+        }
+
+
+        public ActivityProfileDocument GetProfile(Uri activityId, string profileId)
+        {
+            if (profileId == null) return null;
+            return new ActivityProfileDocument();
+        }
+
+        public string[] GetProfiles(Uri activityId)
+        {
+            return new string[0];
+        }
+    }
+}
