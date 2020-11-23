10336bffe7b29c29199c734e71f3146078802691
Emanuel Adrian Lucaci <manu_bc2004@hotmail.com>
2020-11-23 08:19:17 +0200

    Add & Implement AgentProfileDocument

---
+++ xApi/Data/Documents/AgentProfileDocument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace xApi.Data.Documents
+{
+    public class AgentProfileDocument : Document
+    {
+        public AgentProfileDocument(byte[] body, string contentType) : base(body, contentType)
+        {
+        }
+        public AgentProfileDocument() { }
+
+        public int Id { get; set; }
+        public Agent Agent { get; set; }
+
+        public string ProfileId { get; set; }
+    }
+}
