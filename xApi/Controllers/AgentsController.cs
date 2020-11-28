4251bd7d5871e682e92ba49d0d48523f7e399329
Emanuel Adrian Lucaci <manu_bc2004@hotmail.com>
2020-11-28 17:22:29 +0200

    Implement Person model and AgentRepository

---
+++ xApi/Controllers/AgentsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace xApi.Controllers
+{
+    public class AgentsController : XapiBaseController
+    {
+        AgentRepository _agentRepository;
+        public AgentsController()
+        {
+            _agentRepository = new AgentRepository();
+        }
+    }
+}
---
+++ xApi/Repositories/AgentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using xApi.Data;
+
+namespace xApi.Repositories
+{
+    public class AgentRepository
+    {
+
+       Person getPerson(Agent agent)
+        {
+            var person = new Person(agent);
+            return person;
+        }
+    }
+}
