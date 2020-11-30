134e3e80cfd68641b604091efc5df4c1dbd28b6b
Emanuel Adrian Lucaci <manu_bc2004@hotmail.com>
2020-11-30 12:09:59 +0200

    Add connection string and DbUtils class
    Get the connection string with DbUtils helper methods

---
+++ xApi/ApiUtils/DbUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace xApi.ApiUtils
+{
+    public static class DbUtils
+    {
+        public static string GetConnectionString()
+        {
+            // Put the name the Sqlconnection from WebConfig..
+            return ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+        }
+    }
+}
